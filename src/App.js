9f9a9d9c29cade9fc3d91f8a8ac50da6a9922527
eddwordd <edward.plasencio@gmail.com>
2024-03-08 23:23:40 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <p>This is Eddwords application does this work </p>
+        <p>This is Eddwords application does this work testing acitons</p>
         <a
           className="App-link"
           href="https://reactjs.org"
